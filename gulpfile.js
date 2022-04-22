d22c3291ad1b990bf8c19a9b55d70030406ffaf3
ArtemGonchukov <bulkivtulki@gmail.com>
2022-04-22 17:03:46 +0300

    mod_gulp

--- gulpfile.js
+++ gulpfile.js
@@ -27,11 +27,11 @@ gulp.task('styles', function() {
             .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest("src/css"))
             .pipe(browserSync.stream());
-})
+});
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"))
-    gulp.watch("src/*.html").on('change', browserSync.reload)
-})
+    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
